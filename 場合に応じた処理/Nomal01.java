8215b97d0f1a61eabe7476bc96a4e6c68eab3759
いしだ　ゆう <yuishida@ishidayuu-no-Mac-mini.local>
2018-12-11 16:38:06 +0900

    場合に応じた処理問１-4箇所目修正

--- 場合に応じた処理/Nomal01.java
+++ 場合に応じた処理/Nomal01.java
@@ -17,31 +17,30 @@ public class Nomal01 {
 	* @param args コマンドライン引数
 	*/
 	public static void main(String[] args)throws Exception{
-         //文字入力の準
+        //文字入力の準備
 	    InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(input);
 
-         System.out.println("1~10の整数を入力してください");
+        System.out.println("1~10の整数を入力してください");
 
-            //入力を読み込む
-            String number = br.readLine();
+        //入力を読み込む
+        String number = br.readLine();
 
-            //文字列を数値に変えiに代入
-            int i = Integer.parseInt(number);
+        //文字列を数値に変えiに代入
+        int i = Integer.parseInt(number);
 
-            //if文を使い条件分岐
-            if(i > 0 && i <= 10){
-            	if(i > 0 && i < 5){
-            		System.out.println("5未満の整数" + i + "が入力されました。");
-            	}else if(i == 5){
-                  System.out.println("5が入力されました。");
-            	}else{
-            		System.out.println("6以上の整数" + i + "が入力されました。");
-                }
-
+        //if文を使い条件分岐
+        if (i > 0 && i <= 10){
+            if (i > 0 && i < 5){
+            	System.out.println("5未満の整数" + i + "が入力されました。");
+            }else if (i == 5){
+                System.out.println("5が入力されました。");
             }else{
-                    System.out.println("1~10の整数を入力してください！！");
-                }
+            	System.out.println("6以上の整数" + i + "が入力されました。");
+            }
+        }else{
+            System.out.println("1~10の整数を入力してください！！");
+        }
 
-	     }
+	}
 }
