f4228fb54bf4a96e7162fe84cf27d34432ba024f
いしだ　ゆう <yuishida@ishidayuu-no-Mac-mini.local>
2018-12-11 17:33:58 +0900

    場合に応じた処理.問2-6箇所目修正

--- 場合に応じた処理/Nomal02.java
+++ 場合に応じた処理/Nomal02.java
@@ -18,42 +18,42 @@ public class Nomal02 {
 	*/
 	public static void main(String[] args){
 
-      int grading = Integer.parseInt(args[0]);
+    	int grading = Integer.parseInt(args[0]);
 
 
 		switch (grading){
-		  case 1:
-		    System.out.println("もっとがんばりましょう");
-		    break;
-		  case 2:
-		    System.out.println("もう少し頑張りましょう");
-		    break;
-		  case 3:
-		    System.out.println("さらに上を目指しましょう");
-		    break;
-		  case 4:
-		    System.out.println("大変よくできました");
-		    break;
-		  case 5:
-		    System.out.println("大変優秀です");
-		    break;
-		  case 6:
-		    System.out.println("あきらめたらそこで試合終了ですよ");
-		    break;
-		  case 7:
-		    System.out.println("あきらめたらそこで試合終了ですよ");
-		    break;
-		  case 8:
-		    System.out.println("あきらめたらそこで試合終了ですよ");
-		    break;
-		  case 9:
-		    System.out.println("あきらめたらそこで試合終了ですよ");
-		    break;
-		  case 10:
-		    System.out.println("あきらめたらそこで試合終了ですよ");
-		    break;
+		case 1:
+			System.out.println("もっとがんばりましょう");
+			break;
+		case 2:
+			System.out.println("もう少し頑張りましょう");
+			break;
+		case 3:
+			System.out.println("さらに上を目指しましょう");
+			break;
+		case 4:
+			System.out.println("大変よくできました");
+			break;
+		case 5:
+			System.out.println("大変優秀です");
+			break;
+		case 6:
+			System.out.println("あきらめたらそこで試合終了ですよ");
+			break;
+		case 7:
+			System.out.println("あきらめたらそこで試合終了ですよ");
+			break;
+		case 8:
+			System.out.println("あきらめたらそこで試合終了ですよ");
+			break;
+		case 9:
+			System.out.println("あきらめたらそこで試合終了ですよ");
+			break;
+		case 10:
+			System.out.println("あきらめたらそこで試合終了ですよ");
+			break;
 
-	}
+		}
 
 	}
 }
