92f2b74f877ff5b68c17c4080fae8f74b179e06e
いしだ　ゆう <yuishida@ishidayuu-no-Mac-mini.local>
2018-12-11 19:38:34 +0900

    配列.問1の全て修正

--- 配列/Practice_array.java
+++ 配列/Practice_array.java
@@ -3,8 +3,6 @@
 * @author yu-ishida
 */
 
-//文字入力に必要なパッケージをインポートする
-import java.util.Scanner;
 /**
 * Practice_arrayクラス
 * 配列に格納を行うクラス
@@ -17,14 +15,14 @@ public class Practice_array{
   */
   public static void main(String[] args){
     //配列に格納
-    int[]num= {20,30,45,35};
+    int[] num= {20,30,45,35};
 
-  	int x = 1;
+  	int number = 1;
 
     // 繰り返し処理
-    for(int a : num){
-      System.out.println(x+"番目の社員の点数は"+a+"点です。");
-      x++;
+    for(int result : num){
+      System.out.println(number+"番目の社員の点数は"+result+"点です。");
+      number++;
     }
     System.out.println("過去問を解いた社員の人数は全部で"+num.length+"人です。");
   }
