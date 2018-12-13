9bba3e35dfbeeeb4ff085bb2c201b4a228c67146
いしだ　ゆう <yuishida@ishidayuu-no-Mac-mini.local>
2018-12-13 17:07:50 +0900

    インターフェース終了

---
+++ インターフェイス/Practice01_abst.java
@@ -0,0 +1,46 @@
+/**
+* インターフェース-抽象クラス
+* @author yu-ishida
+*/
+
+/**
+* Faceクラス
+* 抽象クラスを持つクラス
+*/
+abstract class Face{
+	/**
+	* noseメソッド
+	* 抽象メソッド
+	*/
+	abstract void nose();
+}
+
+class Face2 extends Face{
+	/**
+	* noseメソッドをオーバーライドするメソッド
+	*/
+	@Override
+	public void nose(){
+
+		System.out.println("鼻を取ります。");
+	}
+}
+
+/**
+* Practice01_abstractクラス
+* noseメソッドを実行するためのクラス
+*/
+public class Practice01_abst{
+	/**
+	* mainメソッド
+	* noseメソッドを実行するメソッド
+	*/
+	public static void main(String[] args){
+		//インスタンス化
+		Face2 action = new Face2();
+
+		action.nose();
+
+	}
+
+}
---
+++ インターフェイス/Practice_inter.java
@@ -0,0 +1,29 @@
+/**
+* インターフェース
+* @author yu-ishida
+*/
+
+/**
+* Faceクラス
+* インターフェースクラス
+*/
+public interface Face{
+	/**
+	* mouthメソッド
+	* 抽象メソッド
+	*/
+	public abstract String mouth();
+	/**
+	* eyeメソッド
+	* 抽象メソッド
+	* @param number //値は数値
+	*/
+	public abstract int eye(int number);
+	/**
+	* noseメソッド
+	* 抽象メソッド
+	*/
+	public abstract void nose();
+}
+
+
