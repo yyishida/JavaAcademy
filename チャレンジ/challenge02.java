d895ab4c372aed844d2a9ffa170be89559d484db
いしだ　ゆう <yuishida@ishidayuu-no-Mac-mini.local>
2018-12-17 16:51:28 +0900

    とりあえず

---
+++ チャレンジ/challenge02.java
@@ -0,0 +1,19 @@
+/**
+* チャレンジ問題＿ピラミッド作成
+* @author yu-ishida
+*/
+
+// 図を作るクラス.
+public class challenge02{
+	/**
+	* mainメソッド
+	* @param args コマンドライン引数
+	*図を作るメソッド.
+	*/
+	public static void main(String args[]){
+        System.out.println("      "+"◻️");
+        System.out.println("    "+"◻️"+"  "+" "+"◻️");
+		System.out.println("  "+"◻️"+" "+"◻️"+" "+"◻️"+" "+"◻️"+" "+"◻️");
+		System.out.println("◻️"+" "+"◻️"+" "+"◻️"+" "+"◻️"+" "+"◻️"+" "+"◻️"+" "+"◻️");
+	}
+}
