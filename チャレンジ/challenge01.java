b45119801b0d2b4c45bb36f294209524474a86e4
いしだ　ゆう <yuishida@ishidayuu-no-Mac-mini.local>
2018-12-18 18:47:23 +0900

    チャレンジ問1修正完了

--- チャレンジ/challenge01.java
+++ チャレンジ/challenge01.java
@@ -2,77 +2,91 @@
 * チャレンジ問題＿場合に応じた処理
 * @author yu-ishida
 */
-
+//ランダムさせるクラスをインポート
+import java.util.Random;
 // コンピュータによる擬似プレイヤー2人によるじゃんけんを一回行うクラス.
 public class challenge01{
-	/**
-	* mainメソッド
-	* @param args コマンドライン引数
-	*ジャンケンをして結果を出力するメソッド
-	*/
-	public static void main(String args[]){
-		//ジャンケン
-		System.out.println("ジャンケンを開始します。");
 
-		int player1 = new java.util.Random().nextInt(3)+1;
+
+	public static void main(String args[]){
+
+		//定数化
+		final String GUU = "グー";
+		final String CHOKI = "チョキ";
+		final String PAA = "パー";
+
+		//ランダムな値を作り、変数に値を入れる
+	    int player1 = new java.util.Random().nextInt(3)+1;
 
 		int player2 = new java.util.Random().nextInt(3)+1;
 
+		//手を格納する
+		String hands = "";
+
+		String handss = "";
+
+
+
 		if(player1 == 1){
+			hands = GUU;
+		}else if(player1 == 2){
+			hands = CHOKI;
+		}else if(player1 == 3){
+			hands = PAA;
+        }
+
+        if(player2 == 1){
+			handss = GUU;
+		}else if(player2 == 2){
+			handss = CHOKI;
+		}else if(player2 == 3){
+			handss = PAA;
+        }
+
+
+	    String jadge = "";
+
+
+		if(player1 == player2){
+			jadge = "あいこです。";
+		}else{
+			if(player1 == 1){
 				switch(player2){
-					case 1:
-				       	System.out.println("player1:グー");
-				       	System.out.println("player2:グー");
-				       	System.out.println("あいこです。");
-				        break;
 				    case 2:
-				    	System.out.println("player1:グー");
-				       	System.out.println("player2:チョキ");
-				       	System.out.println("player1の勝ちです。");
+				       	jadge = "player1の勝ちです。";
 				        break;
 				    case 3:
-				    	System.out.println("player1:グー");
-				       	System.out.println("player2:パー");
-				       	System.out.println("player2の勝ちです。");
+				       	jadge = "player2の勝ちです。";
 			            break;
-		　　　　　}
-		}else if(player1 == 2){
-			switch(player2){
-				case 1:
-			       	System.out.println("player1:チョキ");
-			       	System.out.println("player2:グー");
-			       	System.out.println("player2の勝ち。");
+	            }
+			}else if(player1 == 2){
+				switch(player2){
+					case 1:
+				       	jadge = "player2の勝ちです。";
+				        break;
+				    case 3:
+				       	jadge = "player1の勝ちです。";
+			            break;
+				}
+			}else if(player1 == 3){
+				switch(player2){
+					case 1:
+			       	jadge = "player1の勝ちです。";
 			        break;
 			    case 2:
-			    	System.out.println("player1:チョキ");
-			       	System.out.println("player2:チョキ");
-			       	System.out.println("あいこです。");
+			       	jadge = "player2の勝ちです。";
 			        break;
-			    case 3:
-			    	System.out.println("player1:チョキ");
-			       	System.out.println("player2:パー");
-			       	System.out.println("player1の勝ちです。");
-		        	break;
-			}
-		}else if(player1 == 3){
-				switch(player2){
-					case 1:
-				       	System.out.println("player1:パー");
-				       	System.out.println("player2:グー");
-				       	System.out.println("player1の勝ちです。");
-				    	break;
-				    case 2:
-				    	System.out.println("player1:パー");
-				       	System.out.println("player2:チョキ");
-				       	System.out.println("player2の勝ちです。");
-				    	break;
-				    case 3:
-				    	System.out.println("player1:パー");
-				       	System.out.println("player2:パー");
-				       	System.out.println("あいこです。");
-			        	break;
 		    	}
+			}
 		}
+		//ジャンケン出力
+		System.out.println("ジャンケンを開始します。");
+
+		System.out.println("player1:" + hands);
+
+		System.out.println("player2:" + handss);
+
+		System.out.println(jadge);
     }
 }
 
